8a7fc79ba9ad505a2a62364a7d521f1e5dd783e4
Cameron Hardy <camfhardy@gmail.com>
2017-09-16 22:29:30 -0600

    began implementing active selection, overlap detection, and ghosting while dragging

--- js/playState.js
+++ js/playState.js
@@ -8,29 +8,40 @@ PlayState.init = function () {
 
 PlayState.create = function () {
 	// create level
-	var background = game.add.sprite(0, 0, 'background');
+	let background = game.add.sprite(0, 0, 'background');
 	background.width = game.width;
 	background.height = game.height;
 
 	// TODO: need one array of ships (player's and opponent's)
 	// for now place them manually
-	var ship1 = game.add.sprite(100, 100, 'ship');
-	ship1.anchor.set(0.5, 0.5);
-	ship1.width = 50;
-	ship1.height = 37;
-	ship1.inputEnabled = true;
-	ship1.input.enableDrag();
-	ship1.input.pixelPerfectClick = true;
-	ship1.input.boundsRect = background;
+	this.ship1 = game.add.sprite(100, 100, 'ship');
+	this.ship1.anchor.set(0.5, 0.5);
+	this.ship1.width = 50;
+	this.ship1.height = 37;
+	this.ship1.inputEnabled = true;
+	this.ship1.input.enableDrag(false, true);
+	this.ship1.input.pixelPerfectClick = true;
+	this.ship1.input.boundsRect = background;
+	game.physics.enable(this.ship1);
+	this.ship1.events.onInputDown.add(function () {this.ship1.alpha = 0.3; this.ship1.body.enable = false;}, this);
+	this.ship1.events.onInputUp.add(function () {this.ship1.alpha = 1; this.ship1.body.enable = true;}, this);
 
-	var ship2 = game.add.sprite(150, 100, 'ship');
-	ship2.inputEnabled = true;
-	ship2.input.enableDrag();
-	ship2.input.pixelPerfectClick = true;
-	ship2.input.boundsRect = background;
+	this.ship2 = game.add.sprite(150, 100, 'ship');
+	this.ship2.inputEnabled = true;
+	this.ship2.input.enableDrag(false, true);
+	this.ship2.input.pixelPerfectClick = true;
+	this.ship2.input.boundsRect = background;
+	game.physics.enable(this.ship2);
+	this.ship2.events.onInputDown.add(function () {this.ship2.alpha = 0.3; this.ship2.body.enable = false;}, this);
+	this.ship2.events.onInputUp.add(function () {this.ship2.alpha = 1; this.ship2.body.enable = true;}, this);
 };
 
 PlayState.update = function () {
 	// handle input
 	// update stuff
+	game.physics.arcade.overlap(this.ship1, this.ship2, this._resolveOverlap, null, this);
+}
+
+PlayState._resolveOverlap = function () {
+	console.log('Overlap AAAAAAAAAAAAA');
 }
